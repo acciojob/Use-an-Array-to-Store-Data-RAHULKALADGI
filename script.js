51ba1c3709bda0ccbfef19b270b84fe234bfce37
Rahul R Kaladgi <127622361+RAHULKALADGI@users.noreply.github.com>
2023-04-13 21:46:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-
+let yourArray = [
+	[
+		{
+			one : "String",
+			two : 2,
+			three : true
+		}
+	]
+		[
+		{
+			one : "String",
+			two : 2,
+			three : true
+		}
+	]
+		[
+		{
+			one : "String",
+			two : 2,
+			three : true
+		}
+	]
+		[
+		{
+			one : "String",
+			two : 2,
+			three : true
+		}
+	]
+		[
+		{
+			one : "String",
+			two : 2,
+			three : true
+		}
+	]
+]
